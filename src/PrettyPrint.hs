e1b9941cb50503b89340399703a74bf8ab75ee09
Scott Dubinsky <smdubinsky@gmail.com>
2018-11-01 11:04:33 +0200

    add pretty printer

---
+++ src/PrettyPrint.hs
@@ -0,0 +1,18 @@
+module PrettyPrint where
+import Parser
+prettyPrint :: Int -> JValue -> String
+prettyPrint i (JString s) = s
+prettyPrint i (JNumber num) = show num
+prettyPrint i (JArray arr) =
+  let indent = replicate i '\t'
+      lineIndent = replicate (i+1) '\t'
+  in
+    "[\n" ++ lineIndent ++ ( concat $ map (++ "\n" ++ lineIndent) $ map (prettyPrint (i + 1)) arr) ++ indent ++ "]"
+prettyPrint i (JObject os) =
+  "{\n" ++ printObjects (i+1) os ++ "}"
+
+printObjects :: Int -> [(String, JValue)] -> String
+printObjects _ [] = ""
+printObjects i ((k, v):os) =
+  let indent = replicate i '\t' in
+  indent ++ k ++ ": " ++ prettyPrint i v ++ "\n" ++ printObjects i os
